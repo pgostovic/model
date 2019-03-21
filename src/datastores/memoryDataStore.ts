da6c6cbd91169f84197c0d2368e89a113f3a60a1
Patrick Gostovic <patrick.gostovic@instacart.com>
2019-03-21 14:34:54 -0400

    Added some logging

--- src/datastores/memoryDataStore.ts
+++ src/datastores/memoryDataStore.ts
@@ -26,19 +26,30 @@ export const memoryDataStore = {
     const records = getCollection(modelName).filter(record => record.id !== id);
     const newRecord = { ...data, id };
     collections.set(modelName, [...records, newRecord]);
+    log(`SAVE - ${modelName}(${id})`);
     return id;
   },
 
-  find: async (modelName: string, id: string): Promise<IData | undefined> =>
-    getCollection(modelName).find(record => record.id === id),
+  find: async (modelName: string, id: string): Promise<IData | undefined> => {
+    const record = getCollection(modelName).find(r => r.id === id);
+    log(`FIND - ${modelName}(${id}) ${record ? 'found' : 'not found'}`);
+    return record;
+  },
 
   search: async (
     modelName: string,
     query: IMemoryDataStoreQuery,
-  ): Promise<IData[]> =>
-    getCollection(modelName).filter(
+  ): Promise<IData[]> => {
+    const records = getCollection(modelName).filter(
       record => !Object.keys(query).find(k => record[k] !== query[k]),
-    ),
+    );
+    log(
+      `SEARCH - ${modelName}(${JSON.stringify(query)}) ${
+        records.length
+      } records`,
+    );
+    return records;
+  },
 };
 
 export const logCollections = () => {
