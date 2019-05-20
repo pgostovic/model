7d35acca34d89c370558a2f03fa3e3b0301c403d
Patrick Gostovic <patrick.gostovic@instacart.com>
2019-05-20 06:53:06 -0400

    Added coverage checks

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,19 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[tj]sx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    },
+    "./src/**/*.ts": {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
